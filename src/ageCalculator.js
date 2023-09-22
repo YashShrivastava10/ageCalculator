582e9815dc689f8877b19af5fd4be13574750ccc
YashShrivastava10 <shrivastavayash10@gmail.com>
2023-09-22 06:00:44 +0530

    Handled edge cases

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -95,17 +95,17 @@ function AgeCalculator() {
           <div className='first-row'>
             <div className='day'>
               <label htmlFor='day'>DAY</label>
-              <input id='day' name='day' placeholder='DD'></input>
+              <input id='day' min="1" max="31" type="number" inputMode="numeric" name='day' placeholder='DD'></input>
               {dayError && <span>This field is required.</span>}
             </div>
             <div className='month'>
               <label htmlFor='month'>MONTH</label>
-              <input id='month' name='month' placeholder='MM'></input>
+              <input id='month' min="1" max="12" type="number" inputMode="numeric" name='month' placeholder='MM'></input>
               {monthError && <span>This field is required.</span>}
             </div>
             <div className='year'>
               <label htmlFor='year'>YEAR</label>
-              <input id='year' name='year' placeholder='YYYY'></input>
+              <input id='year' min="1" max="2023" type="number" inputMode="numeric" name='year' placeholder='YYYY'></input>
               {yearError && <span>This field is required.</span>}
             </div>
           </div>
